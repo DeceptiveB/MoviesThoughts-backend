da95468c3d0f5227e9ef6a7f2a6cbf0a5731fe7d
deceptiveb <berny0996@gmail.com>
2024-01-20 21:04:39 -0500

    Fitst commit

---
+++ src/test/java/com/deceptiveb/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deceptiveb.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
